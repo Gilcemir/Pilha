db7fc4bfe50b52ef3cda4bd0809d83b1233d71c6
Gilcemir <gangelofilho@gmail.com>
2022-01-06 10:24:20 -0300

    primeiro commit

---
+++ Pile/Pile.cs
@@ -0,0 +1,35 @@
+﻿namespace Pile;
+public class MyPile
+{
+    Obj topo;
+
+    public void StackUp(object obj)
+    {
+        Obj obj1 = new Obj(this.topo, obj);
+        this.topo = obj1;
+
+    }
+
+    public object Unstack()
+    {
+        if(this.topo == null){
+            throw new ArgumentException("Empty Stack!");
+        }
+        Obj obj1 = this.topo;
+        this.topo = obj1.anterior;
+        return obj1.atual;
+    }
+
+
+    public class Obj
+    {
+        public Obj anterior {get; set;} 
+        public object atual {get; set;}
+
+        public Obj(Obj anterior, object atual){
+            this.atual = atual;
+            this.anterior = anterior;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Pile;
+class Pilha
+{
+    static void Main(string[] args)
+    {
+       MyPile pilhaTeste = new MyPile();
+
+        pilhaTeste.StackUp(1);
+        pilhaTeste.StackUp(2);
+        pilhaTeste.StackUp(3);
+        Console.WriteLine(pilhaTeste.Unstack());
+        Console.WriteLine(pilhaTeste.Unstack());
+        Console.WriteLine(pilhaTeste.Unstack());
+        Console.WriteLine(pilhaTeste.Unstack());
+    }
+}
